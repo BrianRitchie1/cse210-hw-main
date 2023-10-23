89608c223ef5cacc3ea746a4e16357926ac7a7ab
Brian Ritchie <rit20002@byui.edu>
2023-10-23 12:07:26 -0600

    It can run now

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -1,10 +1,105 @@
 using System;
-using System.Diagnostics;
+using System.Collections.Generic;
+using System.Linq;
 
-class Program
+public class BTR_WORD
 {
-    static void Main()
+    public string scripture {get; private set;}
+
+    public bool Is_Dead {get; private set;}
+
+    public BTR_WORD (string scripture1)
     {
-        
+        scripture = scripture1;
+        Is_Dead = false;
     }
+
+    public void Kill()
+    {
+        Is_Dead = true;
+    }
+
+    public override string ToString()
+    {
+        return Is_Dead ? "_____" : scripture;
+    } 
+
+}
+public class SCR_REF
+{
+    public string Reference {get; private set;}
+    public SCR_REF(string reference)
+    {
+        Reference = reference;
+    }
+
+    public override string ToString()
+    {
+        return Reference;
+    }
+
+
+}
+public class Scripture_CNT
+{
+    public SCR_REF Reference {get; private set;}
+
+    public List<BTR_WORD> Content {get; private set;}
+
+    public Scripture_CNT (SCR_REF reference, string scripture)
+    {
+        Reference = reference;
+        Content = scripture.Split(' ').Select(word => new BTR_WORD(word)).ToList();
+    }
+
+    public void Firing_Squad()
+    {
+        Random random = new Random();
+        List<BTR_WORD> visibleContent = Content.Where(word => !word.Is_Dead).ToList();
+        int index = random.Next(visibleContent.Count);
+        visibleContent[index].Kill();
+    }   
+
+    public bool NoLivingLeft()
+    {
+        return Content.All(word => word.Is_Dead);
+    }
+
+    public override string ToString()
+    {
+        return $"{Reference}\n\n{string.Join(" ", Content)}";
+    }
+
+}
+
+public class Program1
+{
+    public static void Main()
+    {
+        Console.Write("What is the name of the scripture? ");
+        string referenceInput = Console.ReadLine();
+        SCR_REF reference = new SCR_REF(referenceInput);
+
+        Console.Write("Enter scripture text: ");
+        string context = Console.ReadLine();
+        Scripture_CNT scripture = new Scripture_CNT(reference, context);
+
+        while (!scripture.NoLivingLeft())
+        {
+            Console.Clear();
+            Console.WriteLine(scripture);
+            Console.WriteLine("\nPress Enter to continue or type 'quit' to exit.");
+
+            string input = Console.ReadLine();
+            if (input.ToLower() == "quit")
+            {
+                break;
+            }
+
+            scripture.Firing_Squad();
+        }
+
+    }
+
+
 }
--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -1,9 +1,52 @@
 using System;
+using System.Threading;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Develop04 World!");
+        Console.WriteLine("Loading...");
+        Spinner spinner = new Spinner();
+        spinner.Start();
+        // Simulate some loading work (replace this with your actual loading code)
+        Thread.Sleep(5000);
+        spinner.Stop();
+        Console.WriteLine("Loading complete!");
+    }
+}
+
+class Spinner
+{
+    private bool _stop = false;
+
+    public void Start()
+    {
+        _stop = false;
+        Thread thread = new Thread(Spin);
+        thread.Start();
+    }
+
+    public void Stop()
+    {
+        _stop = true;
+    }
+
+    private void Spin()
+    {
+        while (!_stop)
+        {
+            Console.Write("/");
+            Thread.Sleep(100);
+            Console.SetCursorPosition(Console.CursorLeft - 1, Console.CursorTop);
+            Console.Write("-");
+            Thread.Sleep(100);
+            Console.SetCursorPosition(Console.CursorLeft - 1, Console.CursorTop);
+            Console.Write("\\");
+            Thread.Sleep(100);
+            Console.SetCursorPosition(Console.CursorLeft - 1, Console.CursorTop);
+            Console.Write("|");
+            Thread.Sleep(100);
+            Console.SetCursorPosition(Console.CursorLeft - 1, Console.CursorTop);
+        }
     }
 }
